284d081b96fc1dd4336083c6af50756c0f7a7c90
hakagoo <shm6155@gmail.com>
2019-09-06 14:18:55 +0900

    1212 / 5218

---
+++ Algorithm/src/P_5218.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class P_5218 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = Integer.parseInt(sc.nextLine());
+		String result = "";
+		
+		for(int i = 0; i < n; i++) {
+			result += "Distances: ";
+			String[] input = sc.nextLine().split(" ");
+			char[] first = input[0].toCharArray();
+			char[] second = input[1].toCharArray();
+			
+			for(int j = 0; j < first.length; j++) {
+				if(first[j] <= second[j]) {
+					result += second[j]-first[j] + " ";
+				} else {
+					result += second[j]-first[j] + 26 + " ";
+				}
+			}
+			result += "\n";
+		}
+		System.out.println(result);
+	}
+}
+
