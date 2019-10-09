82b280f330727ea3ff25e385e222d45afd8a1e15
hakagoo <shm6155@gmail.com>
2019-10-09 16:52:31 +0900

    피보나치 - 표

--- bj_algorithm/src/P_1003.java
+++ bj_algorithm/src/P_1003.java
@@ -1,33 +1,23 @@
 import java.util.Scanner;
 
 public class P_1003 {
-	
-	static int[] arr = new int[2];
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = Integer.parseInt(sc.nextLine());
-		String result = "";
+		int[][] dp = new int[41][2];
+		dp[0][0] = dp[1][1] = 1;
 		
+		for(int i = 2; i <= dp.length-1; i++) {
+			dp[i][0] = dp[i-1][0] + dp[i-2][0];
+			dp[i][1] = dp[i-1][1] + dp[i-2][1];
+		}
+		
+		String result = "";
 		for(int i = 0; i < t; i++) {
-			arr[0] = 0;
-			arr[1] = 0;
 			int n = Integer.parseInt(sc.nextLine());
-			fibo(n);
-			result += arr[0] + " " + arr[1] + "\n";
+			result += dp[n][0] + " " + dp[n][1] + "\n";
 		}
+		
 		System.out.println(result);
-	}
-	
-	public static int fibo(int n) {
-		if(n == 0) {
-			arr[0]++;
-			return 0;
-		} else if(n == 1) {
-			arr[1]++;
-			return 1;
-		} else {
-			return fibo(n-1) + fibo(n-2);
-		}
 	}
 }
