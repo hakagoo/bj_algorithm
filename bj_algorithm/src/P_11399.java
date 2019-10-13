34ddad65c476798a664b1f1ab093e2e2fa8d6f2f
hakagoo <shm6155@gmail.com>
2019-10-13 19:41:51 +0900

    ATM -DP

--- bj_algorithm/src/P_11399.java
+++ bj_algorithm/src/P_11399.java
@@ -5,21 +5,23 @@ public class P_11399 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		int[] arr = new int[n];
+		int[] arr = new int[n+1];
 		
-		for(int i = 0; i < n; i++) {
+		for(int i = 1; i <= n; i++) {
 			arr[i] = sc.nextInt();
 		}
 		
 		Arrays.sort(arr);
-		int[] p = new int[n];
+		int[] p = new int[n+1];
 		int min = 0;
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < i+1; j++) {
-				p[i] += arr[j];
+		if(n >= 1) {
+			p[1] = arr[1];
+			min = p[1];
+			for(int i = 2; i <= n; i++) {
+				p[i] = p[i-1] + arr[i];
+				min += p[i];
 			}
-			min += p[i];
 		}
 		
 		System.out.println(min);
