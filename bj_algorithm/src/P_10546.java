02748a9bbc9254dacbaf6723a13d297c5d219f5a
hakagoo <shm6155@gmail.com>
2019-10-16 00:15:46 +0900

    배부른마라토너

--- bj_algorithm/src/P_10546.java
+++ bj_algorithm/src/P_10546.java
@@ -15,7 +15,11 @@ public class P_10546 {
         
         for(int i = 0; i < n; i++){
             String input = sc.nextLine();
-            map.put(input, 1);
+            if(map.containsKey(input)){
+                map.put(input, map.get(input)+1);
+            } else {            	
+            	map.put(input, 1);
+            }
         }
         
         for(int i = 0; i < n-1; i++){
@@ -27,13 +31,15 @@ public class P_10546 {
         Set<Entry<String, Integer>> entry = map.entrySet();
         Iterator<Entry<String, Integer>> it = entry.iterator();
         
+        String result = "";
         while(it.hasNext()){
             Entry<String, Integer> e = it.next();
             if(e.getValue() % 2 == 1){
-                System.out.println(e.getKey());
+            	result = e.getKey();
                 break;
             }
         }
+        System.out.println(result);
         
 	}
 }
