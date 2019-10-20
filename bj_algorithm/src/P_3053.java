b601b60e3e7c051f9b11d7c53d0aee492efcab6a
hakagoo <shm6155@gmail.com>
2019-10-20 21:55:17 +0900

    택시기하학

---
+++ bj_algorithm/src/P_3053.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P_3053 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double r = sc.nextDouble();
+		System.out.println(r * r * Math.PI);
+		System.out.println(r * r * 2);
+		
+	}
+}
