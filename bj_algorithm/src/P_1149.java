4996e675298db22a665ff23950c1b55d91f58fdb
hakagoo <shm6155@gmail.com>
2019-10-20 16:19:16 +0900

    rgb거리

--- bj_algorithm/src/P_1149.java
+++ bj_algorithm/src/P_1149.java
@@ -5,7 +5,7 @@ public class P_1149 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[][] t = new int[n+1][3];
-		int[][] dp = new int[n+1][2];
+		int[][] dp = new int[n+1][3];
 		
 		for(int i = 1; i <= n; i++) {
 			for(int j = 0; j < 3; j++) {
@@ -14,35 +14,15 @@ public class P_1149 {
 		}
 
 		for(int i = 1; i <= n; i++) {
-			if(i == 1) {
-				int min = 0;
-				int minIndex = 0; //0 = r, 1 = g, 2 = b;
-				for(int j = 0; j < 3; j++) {
-					if(j == 0 || min > t[1][j]) {
-						min = t[1][j];
-						minIndex = j;
-					}
-					dp[1][0] = minIndex;
-					dp[1][1] = min;
-				}
-			}
-			
-			if(i >= 2) {				
-				if(dp[i-1][0] == 0) {
-					dp[i][0] = ((t[i][1] > t[i][2]) ? 2 : 1);
-					dp[i][1] += dp[i-1][1] + t[i][dp[i][0]];
-				} else if(dp[i-1][0] == 1) {
-					dp[i][0] = ((t[i][0] > t[i][2]) ? 2 : 0);
-					dp[i][1] += dp[i-1][1] + t[i][dp[i][0]];
-				} else if(dp[i-1][0] == 2) {
-					dp[i][0] = ((t[i][0] > t[i][1]) ? 1 : 0);
-					dp[i][1] += dp[i-1][1] + t[i][dp[i][0]];
-				}
-			}
-			
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + t[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + t[i][1];
+			dp[i][2] = Math.min(dp[i-1][1], dp[i-1][0]) + t[i][2];
 		}
 		
-		System.out.println(dp[n][1]);
+		int result = Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
+		System.out.println(result);
 		
 	}
+	
+	
 }
